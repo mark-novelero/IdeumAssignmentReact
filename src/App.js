e21b60f65da70c2e8116214a104528c5a12f878f
mark novelero <mark.novelero@gmail.com>
2021-11-29 22:59:08 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+class App extends Component {
+ 
+  goToCaliforniaScienceCenter = () => {
+
+    window.open("https://californiasciencecenter.org/");
+  }
+
+
+  goToPalmSpringsTramway = () => {
+
+    window.open("https://pstramway.com/")
+  }
+
+
+  goToSmithsonian = () => {
+
+    window.open("https://africa.si.edu/")
+  }
+
+ 
+ render() {
+  return(
+    <div>
+      <header>
+        <h1>Mark Novelero: Ideum Coding Assignment</h1>
+      </header>  
+      <main className="main-container" >
+        <div className="column-1" >
+          <button type="button" className="column-1-btn" onClick={() => this.goToCaliforniaScienceCenter()} >
+            <img src="https://i.imgur.com/eiM5Bceh.png" alt="California Science Center" className="california-science-img" ></img>
+          </button>
+          <p>California Science Center</p>
+        </div>
+        <div className="column-2" >
+            <button className="column-2-btn" type="button" onClick={() => this.goToPalmSpringsTramway()} >
+                <img src="https://i.imgur.com/DdSaaNOh.png" alt="Palm Springs Aerial Tramway" className="palm-springs-img" ></img> 
+            </button>
+            <p>Palm Springs Aerial Tramway</p>
+            <button className="column-2-btn" type="button" onClick={() => this.goToSmithsonian()} >
+                <img src="https://i.imgur.com/wAYCOa1l.png" alt="Smithsonian National Museum of African Art" className="african-art-img" ></img>
+            </button>
+            <p>Smithsonian National Museum of African Art</p>
+        </div> 
+      </main>
     </div>
-  );
-}
+    )
+   }
+ }
 
-export default App;
+
+
+export default App
